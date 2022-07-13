c53e6b134fc6712eee1354da3cce753b41dce690
ZekiXerxes <56410159+ZekiXerxes@users.noreply.github.com>
2022-07-13 21:14:55 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { Box, Link, useColorMode, jsx } from "theme-ui"
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
 
 const Footer = () => {
   const [colorMode, setColorMode] = useColorMode()
@@ -20,7 +20,38 @@ const Footer = () => {
       </button>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
- 
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        {isDark ? (
+          <img width="30" height="30" src="https://.png" alt="LLogo" />
+        ) : (
+          <img width="30" height="30" src="https://img.png" alt="LLogo" />
+        )}
+        {` `}
+        <Link
+          aria-label="Linky"
+          sx={{ ml: 2 }}
+          href="https://githubL"
+        >
+          Theme
+        </Link>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Link
+          aria-label="Linke"
+          href="https://www.le"
+        >
+          Web0
+        </Link>
+      </Flex>
     </Box>
   )
 }
