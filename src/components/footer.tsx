587dedf8ebbda0ac2ec5d98161f35277582b69e7
ZekiXerxes <56410159+ZekiXerxes@users.noreply.github.com>
2022-07-13 20:55:14 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
       </button>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
-      <Flex
+      <!--<Flex
         sx={{
           justifyContent: `center`,
           alignItems: `center`,
@@ -28,30 +28,30 @@ const Footer = () => {
           color: `text`,
           fontWeight: `semibold`,
           a: { color: `text` },
-        }}
-      >
-        {isDark ? (
+        }}-->
+      <!--> -->
+       <!-- {isDark ? (
           <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2-light_w30.png" alt="LekoArts Logo" />
         ) : (
           <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2_w30.png" alt="LekoArts Logo" />
-        )}
-        {` `}
-        <Link
+        )}-->
+        <!--{` `}-->
+        <!--<Link
           aria-label="Link to the theme's GitHub repository"
           sx={{ ml: 2 }}
           href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara"
         >
           Theme
         </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
+        <div sx={{ mx: 1 }}>by</div>-->
+        <!--{` `}
         <Link
           aria-label="Link to the theme author's website"
           href="https://www.lekoarts.de?utm_source=cara&utm_medium=Theme"
         >
           LekoArts
         </Link>
-      </Flex>
+      </Flex> -->
     </Box>
   )
 }
